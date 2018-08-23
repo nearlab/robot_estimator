da30544b3e637ad82310578adaf234ab937ba6be
James Bell <jtb2013@gmail.com>
2018-08-23 20:04:33 +0000

    hahaha

--- src/cans_controller_node.cpp
+++ src/cans_controller_node.cpp
@@ -26,44 +26,44 @@ void stateCallback(const robot_controller::SpacecraftState msg){
 }
 void setupPWMs(){
   pwmWriter.open("/sys/class/pwm/pwmchip1/export",std::ofstream::out);
-  pwmWriter << 0;
+  pwmWriter << 0 <<"\n";
   pwmWriter.close();
   pwmWriter.open("/sys/class/pwm/pwmchip6/export",std::ofstream::out);
-  pwmWriter << 0;
+  pwmWriter << 0 <<"\n";
   pwmWriter.close();
   pwmWriter.open("/sys/class/pwm/pwmchip4/export",std::ofstream::out);
-  pwmWriter << 0;
+  pwmWriter << 0 <<"\n";
   pwmWriter.close();
 
   ros::Duration(3).sleep();
 
-  pwmWriter.open("/sys/class/pwm/pwmchip1/pmw-1:0/period",std::ofstream::out);
-  pwmWriter << 33333;
+  pwmWriter.open("/sys/class/pwm/pwmchip1/pwm-1:0/period",std::ofstream::out);
+  pwmWriter << 33333 <<"\n";
   pwmWriter.close();
-  pwmWriter.open("/sys/class/pwm/pwmchip1/pmw-1:0/enable",std::ofstream::out);
-  pwmWriter << 1;
-  pwmWriter.close();
-
-  pwmWriter.open("/sys/class/pwm/pwmchip4/pmw-4:0/period",std::ofstream::out);
-  pwmWriter << 33333;
-  pwmWriter.close();
-  pwmWriter.open("/sys/class/pwm/pwmchip4/pmw-4:0/enable",std::ofstream::out);
-  pwmWriter << 1;
+  pwmWriter.open("/sys/class/pwm/pwmchip1/pwm-1:0/enable",std::ofstream::out);
+  pwmWriter << 1 << "\n";
   pwmWriter.close();
 
-  pwmWriter.open("/sys/class/pwm/pwmchip6/pmw-6:0/period",std::ofstream::out);
-  pwmWriter << 33333;
+  pwmWriter.open("/sys/class/pwm/pwmchip4/pwm-4:0/period",std::ofstream::out);
+  pwmWriter << 33333 <<"\n";
   pwmWriter.close();
-  pwmWriter.open("/sys/class/pwm/pwmchip6/pmw-6:0/enable",std::ofstream::out);
-  pwmWriter << 1;
+  pwmWriter.open("/sys/class/pwm/pwmchip4/pwm-4:0/enable",std::ofstream::out);
+  pwmWriter << 1 << "\n";
+  pwmWriter.close();
+
+  pwmWriter.open("/sys/class/pwm/pwmchip6/pwm-6:0/period",std::ofstream::out);
+  pwmWriter << 33333<<"\n";
+  pwmWriter.close();
+  pwmWriter.open("/sys/class/pwm/pwmchip6/pwm-6:0/enable",std::ofstream::out);
+  pwmWriter << 1<<"\n";
   pwmWriter.close();
 }
 int main(int argc, char** argv){
   
   ros::init(argc,argv,"cans_controller");
-  setupPWMs();
   std::string robotNameStr;
   ros::NodeHandle nh;
+  setupPWMs();
   double gamma,alpha,mass,radius,wheelDist,pi;
   pi = 3.14159265358979;
   nh.getParam("gamma",gamma);
@@ -113,14 +113,14 @@ int main(int argc, char** argv){
     u = R.inverse()*(A1*a + A2*v);
     u = u/3.33*33333;
     //Write duty cycle to pwm files
-    pwmWriter.open("/sys/class/pwm/pwmchip1/pmw-1:0/duty_cycle",std::ofstream::out);
-    pwmWriter << u(0);
+    pwmWriter.open("/sys/class/pwm/pwmchip1/pwm-1:0/duty_cycle",std::ofstream::out);
+    pwmWriter << u(0)<<"\n";
     pwmWriter.close();
-    pwmWriter.open("/sys/class/pwm/pwmchip4/pmw-4:0/duty_cycle",std::ofstream::out);
-    pwmWriter << u(1);
+    pwmWriter.open("/sys/class/pwm/pwmchip4/pwm-4:0/duty_cycle",std::ofstream::out);
+    pwmWriter << u(1)<<"\n";
     pwmWriter.close();
-    pwmWriter.open("/sys/class/pwm/pwmchip6/pmw-6:0/duty_cycle",std::ofstream::out);//std::ofstream::append, for logging.
-    pwmWriter << u(2);
+    pwmWriter.open("/sys/class/pwm/pwmchip6/pwm-6:0/duty_cycle",std::ofstream::out);//std::ofstream::append, for logging.
+    pwmWriter << u(2)<<"\n";
     pwmWriter.close();
 
     ros::spinOnce();
